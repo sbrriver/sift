0adc1a3d2ce5bfca781a421a35ae36462cd62241
Auden <auden.nerd@gmail.com>
2023-05-21 17:33:23 -0700

    added tess data download functionality - can adjust so downloading kepler, k2 as well. ran pylint, added documentation. tess data does not use start date parameter - still looking through documentation.

--- ztfapi.py
+++ ztfapi.py
@@ -1,34 +1,54 @@
 import argparse
 from ztfquery import query
 from astropy.time import Time
-import getopt
 from lightkurve import search_targetpixelfile
 
 def get_ztf_data(ra, dec, radius, start_date):
+    """Gets specified data from the ztf database. Only can access public data.
+
+    Args:
+        ra (float): right ascension
+        dec (float): declination
+        radius (float): radius of region of sky to search, with center at (ra, dec), in arcsec
+        start_date (str): starting date to start search for
+    """
     zquery = query.ZTFQuery()
-    
+
     # convert start_date and end_date to MJD
     start_mjd = Time(start_date, format='iso').mjd
-    #end_mjd = Time(end_date, format='iso').mjd
 
     zquery.load_metadata(radec=[ra,dec], size=radius, sql_query=f"fid=3 and obsjd>{start_mjd}")
-    zquery.download_data("sciimg.fits", show_progress=True, nprocess=1, verbose=True, overwrite=False)
+    zquery.download_data("sciimg.fits", show_progress=True, nprocess=1, verbose=True, \
+        overwrite=False)
 
 def get_tess_data(ra, dec, radius, start_date):
-    #lightkurve.search_lightcurve(
-    return 'not yet implemented'
-    
+    """Gets specified data from the tess database. Only can access public data.
+
+    Args:
+        ra (float): right ascension
+        dec (float): declination
+        radius (float): radius of region of sky to search, with center at (ra, dec), in arcsec
+        start_date (str): starting date to start search for
+    """
+    target = str(ra) + " " + str(dec)
+    pixelfile = search_targetpixelfile(target, radius=radius, mission='TESS').download()
+    pixelfile.download_all()
 
 databases = {'ztf':get_ztf_data, 'tess':get_tess_data}
 
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Retrieve ZTF objects within a certain area and time range.')
-    parser.add_argument('--ra', type=float, required=True, help='Right ascension of center of search area.')
-    parser.add_argument('--dec', type=float, required=True, help='Declination of center of search area.')
-    parser.add_argument('--radius', type=float, required=True, help='Radius of search area in degrees.')
-    parser.add_argument('--database', type=str, required=True, help='Database to search. Options: ztf, tess.')
-    parser.add_argument('--start_date', type=str, required=True, help='Start date of search in format YYYY-MM-DD.')
+    parser = argparse.ArgumentParser(description=\
+        'Retrieve ZTF objects within a certain area and time range.')
+    parser.add_argument('--ra', type=float, required=True, \
+        help='Right ascension of center of search area.')
+    parser.add_argument('--dec', type=float, required=True, \
+        help='Declination of center of search area.')
+    parser.add_argument('--radius', type=float, required=True, \
+        help='Radius of search area in degrees.')
+    parser.add_argument('--database', type=str, required=True, \
+        help='Database to search. Options: ztf, tess.')
+    parser.add_argument('--start_date', type=str, required=True, \
+        help='Start date of search in format YYYY-MM-DD.')
     args = parser.parse_args()
 
     # Call the function to retrieve the objects
