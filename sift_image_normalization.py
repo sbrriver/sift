584f9cfb6acf915d8e380d2f8db39fe1df32845e
Leo Raksin <128433792+lraksin@users.noreply.github.com>
2024-02-09 10:12:39 -0800

    Update sift_image_normalization.py
    Crops images around sn

--- sift_image_normalization.py
+++ sift_image_normalization.py
@@ -4,6 +4,7 @@ from astropy.io import fits
 import numpy as np
 from PIL import Image
 import time
+from scipy.ndimage import rotate
 
 def data_scale(img_dat, minval, maxval):
     """Scale image data to [0, 1] range.
@@ -19,7 +20,7 @@ def data_scale(img_dat, minval, maxval):
     #scale data to [0,1] range
     return (img_dat - minval)/(maxval - minval)
 
-def normalize(img_dat, center_ra, center_dec):
+def normalize(img_dat, center_ra, center_dec, sn_ra, sn_dec, orientation, pixscale):
     """Normalize image data to be black and white.
 
     Args:
@@ -31,20 +32,31 @@ def normalize(img_dat, center_ra, center_dec):
     if center_ra != None:
         """Input supernovae ra and dec fits files are downloaded for."""
         """Will account for any tilt of image in a bit."""
-        sn_ra = 0.814286206
-        sn_dec = 16.14570793
-        center = (1536 - round((sn_ra - center_ra) * 3600/1.01), 1540 - round((sn_dec - center_dec) * 3600/1.01))
-        zoom = 60
+        
+        """Account for tilt of image"""
+        tilt = orientation - 180
+        img_dat = rotate(img_dat, tilt)
+        """Should center exactly around sn, center = sn_ra, sn_dec"""
+        center = (1536 - round((sn_ra - center_ra) * 3600/pixscale), 1540 - round((sn_dec - center_dec) * 3600/pixscale))
+        pixel_location_ra = (sn_ra - center_ra) * 3600/pixscale
+        zoom = 64
         img_dat = img_dat[0 if center[1] - zoom < 0 else center[1] - zoom: center[1] + zoom, 0 if center[0] - zoom < 0 else center[0] - zoom: center[0] + zoom] #(y1,y2:x1,x2)
+        if (len(img_dat), len(img_dat[0])) != (zoom*2, zoom*2):
+            return False
         maxval = np.max(img_dat)
         minval = np.min(img_dat)
+        """Image Normalization"""
         result = data_scale(img_dat, minval, maxval)
-        
-        q = 6
+        q = 12
         qth_percentile = np.percentile(result, [q])
         image_data_jpg = (result - qth_percentile) * 2000
         image_data_jpg = np.clip(image_data_jpg, 0, 255).astype(np.uint8)
         image = Image.fromarray(image_data_jpg, 'L')
+        """Without Image Normalization"""
+        result = data_scale(img_dat, minval, maxval) * 255
+        # image_data_jpg = np.clip(result, 0, 255).astype(np.uint8)
+        # image = Image.fromarray(image_data_jpg, 'L')
+        
         return image
     
     else:
@@ -59,63 +71,6 @@ def normalize(img_dat, center_ra, center_dec):
         image_data_jpg = np.clip(image_data_jpg, 0, 255).astype(np.uint8)
         image = Image.fromarray(image_data_jpg, 'L')
         return image
-   
-        """sqrt percentile"""
-        # q = 65
-        # qth_percentile = np.percentile(result, [q])
-        # image_data_jpg = (np.sqrt(result) - np.sqrt(qth_percentile)) * 200000
-        # image_data_jpg = np.clip(image_data_jpg, 0, 255).astype(np.uint8)
-        # image = Image.fromarray(image_data_jpg, 'L')
-        # return image
-
-        """scaling based on white level of image"""
-        """Subtract a number based on the average brightness of the image to remove faint brightness across image. Scale values above this to be brighter."""
-        # zero_value_limit = np.mean(result.mean(axis=1)) * 240
-        # scale_factor = 3000
-        # image_data_jpg = (255*result - zero_value_limit) * scale_factor
-        # """Clip the data to be within 0 to 255 before changing to 8 bit so values above 255 don't flip to lower values."""
-        # image_data_jpg = np.clip(image_data_jpg, 0, 255).astype(np.uint8)
-        # """If the average brightness is over threshold increase the zero_value_limit and rerun code. Lower threshold means this runs more and will take longer."""
-        # threshold = 80
-        # while np.mean(image_data_jpg.mean(axis=1)) > threshold:
-        #     zero_value_limit += 0.02
-        #     image_data_jpg = (255*result - zero_value_limit) * scale_factor
-        #     image_data_jpg = np.clip(image_data_jpg, 0, 255).astype(np.uint8)
-        # image = Image.fromarray(image_data_jpg, 'L')
-        # return image
-
-        """log"""
-        # scale_factor = 5000
-        # image_data_jpg = np.log(scale_factor * result + 1) / np.log(scale_factor) * 255
-        # image_data_jpg = np.clip(image_data_jpg, 0, 255).astype(np.uint8)
-        # image = Image.fromarray(image_data_jpg, 'L')
-        # return image
-
-        """pow"""
-        # scale_factor = 1000
-        # image_data_jpg = (scale_factor ** result - 1) * 255
-        # image_data_jpg = np.clip(image_data_jpg, 0, 255).astype(np.uint8)
-        # image = Image.fromarray(image_data_jpg, 'L')
-        # return image
-
-        """sqrt"""
-        # image_data_jpg = np.sqrt(result) * 500
-        # image_data_jpg = np.clip(image_data_jpg, 0, 255).astype(np.uint8)
-        # image = Image.fromarray(image_data_jpg, 'L')
-        # return image
-
-        """square""" 
-        # image_data_jpg = result ** 2 * 1000000
-        # image_data_jpg = np.clip(image_data_jpg, 0, 255).astype(np.uint8)
-        # image = Image.fromarray(image_data_jpg, 'L')
-        # return image
-
-        """asinh"""
-        # scale_factor = 1000
-        # image_data_jpg = scale_factor * np.sinh(result * 10) / 3
-        # image_data_jpg = np.clip(image_data_jpg, 0, 255).astype(np.uint8)
-        # image = Image.fromarray(image_data_jpg, 'L')
-        # return image
 
 def find_fits_files(directory):
     """Find all .fits files in given directory.
@@ -134,7 +89,7 @@ def find_fits_files(directory):
 
     return fits_files
 
-def data_process(directory, center_ra, center_dec):
+def data_process(directory, center_ra, center_dec, sn_ra, sn_dec, orientation, pixscale):
     """Produces black and white jpgs from all fits files in given directory.
 
     Args:
@@ -159,7 +114,7 @@ def data_process(directory, center_ra, center_dec):
 
         image_name = str(date) + os.path.basename(file).replace('.fits', '.jpg')
 
-        processed_unsorted_data.append([location, date, normalize(image_data, center_ra, center_dec), image_name, filter])
+        processed_unsorted_data.append([location, date, normalize(image_data, center_ra, center_dec, sn_ra, sn_dec, orientation, pixscale), image_name, filter])
 
     for image in processed_unsorted_data:
         location = image[0]
@@ -168,32 +123,40 @@ def data_process(directory, center_ra, center_dec):
 
     return dict(sorted_data)
 
-def data_store(normalized_images):
+def data_store(normalized_images, center_ra, center_dec, sn_ra, sn_dec, version):
     """Go through normalized images and save in subfolder labelled by location.
 
     Args:
         normalized_images (defaultdict): dictionary of image data, with key being location (ra, dec)
         and value being a tuple of (date (string), normalized image (Image type), image name)
-    """
+    """        
     if not os.path.exists("training"):
         os.makedirs("training")
 
     for location in normalized_images.keys():
-        location_path = "training/" + 'ra_'+location[0].replace(':','-').replace('+','p') + 'dec_'+location[1].replace(':','-').replace('+','p')
+        # location_path = "training/" + 'ra_'+location[0].replace(':','-').replace('+','p') + 'dec_'+location[1].replace(':','-').replace('+','p')
+        # if not os.path.exists(location_path):
+        #     os.makedirs(location_path)
+        # for observation in normalized_images[location]:
+        #     filter_path = location_path + "/" + observation[3]
+        #     if not os.path.exists(filter_path):
+        #         os.makedirs(filter_path)
+        #     image = observation[1]
+        #     image_name = observation[2]
+        #     image.save(filter_path + "/" + image_name)
+        """Changed path so each file is downloaded to the supernova it is meant for and to include ra and dec in degrees"""
+        location_path = "training/" + str(sn_ra) + ", " + str(sn_dec)
         if not os.path.exists(location_path):
             os.makedirs(location_path)
         for observation in normalized_images[location]:
-            filter_path = location_path + "/" + observation[3]
-            if not os.path.exists(filter_path):
-                os.makedirs(filter_path)
             image = observation[1]
             image_name = observation[2]
-            image.save(filter_path + "/" + image_name)
+            image.save(location_path + "/" + version + '_' + str(center_ra) + ", " + str(center_dec) + "_" + image_name)
 
 if __name__ == '__main__':
     start_time = time.time()
-    processed_data = data_process(os.getcwd(), None, None)
-    data_store(processed_data)
+    processed_data = data_process(os.getcwd(), None, None, None, None)
+    data_store(processed_data, None, None, None, None)
     end_time = time.time()
     elapsed_time = end_time - start_time
     print("Time to execute statement: " + str(elapsed_time) + 'seconds')
