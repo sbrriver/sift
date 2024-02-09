5e8f71a3177379d4145b2d42e2196977f5e14fb1
Leo Raksin <128433792+lraksin@users.noreply.github.com>
2024-02-09 10:15:39 -0800

    Update astrometry_process.py
    Fixed some errors

--- astrometry_process.py
+++ astrometry_process.py
@@ -8,36 +8,58 @@ from astropy.io import fits
 from astrometry.net.client import Client
 import sift_image_normalization as sin
 import time
+import urllib.error
 
-def astrometry(directory):
+def astrometry(directory: str, sn_ra, sn_dec, client, version):
     """Uploads fits files in directory to astrometry.net, runs sift_image_normalization.
 
     Args:
-        directory (string): path to directory to search for data.
+        directory: path to directory to search for data.
     """
     
     """Login, change api_key to your key found at https://nova.astrometry.net/api_help."""
-    api_key = 'iqzsxmsaxydxbioo'
-    client = Client()
-    client.login(api_key)
+    print("\n Astrometry running on: " + str(directory) + "\n")
+    # api_key = 'iqzsxmsaxydxbioo'
+    # client = Client()
+    # client.login(api_key)
 
-    file_list = sin.find_fits_files(directory)
-    count = 0
-    for file in file_list:
-        count += 1
-        print('\n' + str(count) + '/' + str(len(file_list)) + '\n')
-        upload = client.upload(fn=file)
-        subid = str(upload['subid'])
-        
-        """Getting jobid. If the job_calibrations list is empty, calibration is not complete yet and will wait 10 seconds."""
-        job_calibrations_length = 0
-        while job_calibrations_length == 0:
-            print('\n' + 'Request while loop running')
-            status = requests.post('http://nova.astrometry.net/api/submissions/' + subid, headers={'Referer': 'http://www.example.com'})
-            job_calibrations_length = len(status.json()['job_calibrations'])
-            time.sleep(10)
+    file = sin.find_fits_files(directory)[0]
+    # count = 0
+    # count += 1
+    # print(str(count) + '/' + str(len(file_list)) + ' fits files')
+    retries = 3
+    for i in range(retries):
+        try:
+            upload = client.upload(fn=file)
+            if type(upload) == dict:
+                subid = str(upload['subid'])
+            else:
+                upload = client.upload(fn=file)
+                time.sleep(1)
+                try:
+                    subid = str(upload['subid'])
+                except:
+                    return
+            status = requests.post('http://nova.astrometry.net/api/submissions/' + subid, headers={'Referer': 'http://www.example.com', 'scale_type': 'ev', 'scale_est': '1.0123535066635947', 'scale_err': '1'})
+            break
+        except urllib.error.URLError as e:
+            print(f"Attempt {i+1} failed: {e}")
+            time.sleep(5)
+    else:
+        print("All retry attempts failed. Check your network or server.")
+    time.sleep(10)
+    job_calibrations_length = 0
+    loops = 0
+    """Getting jobid. If the job_calibrations list is empty, calibration is not complete yet and will wait."""
+    while job_calibrations_length == 0 and loops < 40:
+        loops += 1
+        time.sleep(2)
+        status = requests.post('http://nova.astrometry.net/api/submissions/' + subid, headers={'Referer': 'http://www.example.com', 'scale_type': 'ev', 'scale_est': '1.0123535066635947', 'scale_err': '1'})
+        job_calibrations_length = len(status.json()['job_calibrations'])
+    if loops == 40:
+        print('\n\n\n image took too long to solve \n\n\n')
+    else:
         jobid = str(status.json()['jobs'])[1:-1]
-        
         """Getting calibration data"""
         calibration = client.send_request('jobs/%s/calibration' % jobid)
         center_ra = calibration['ra']
@@ -45,20 +67,30 @@ def astrometry(directory):
         # width_arcsec = calibration['width_arcsec']
         # height_arcsec = calibration['height_arcsec']
         # radius = calibration['radius']
-        # orientation = calibration['orientation']
+        pixscale = calibration['pixscale']
+        orientation = calibration['orientation']
         # annotations = client.send_request('jobs/%s/annotations' % jobid)
         # info = client.send_request('jobs/%s/info' % jobid)
         # client.annotate_data(jobid)
         # client.job_status(jobid)
-        print(center_ra, center_dec)
-        
+        print("Image center: (" + str(center_ra) + ", " + str(center_dec) + ")")
         """Image normalization"""
-        processed_data = sin.data_process(file[0:60], center_ra, center_dec)
-        sin.data_store(processed_data)
-    
+
+        try:
+            processed_data = sin.data_process(file[:-49], center_ra, center_dec, sn_ra, sn_dec, orientation, pixscale)
+            sin.data_store(processed_data, center_ra, center_dec, sn_ra, sn_dec, version)
+        except Exception as e:
+            if e is KeyboardInterrupt:
+                exit()
+            print("\n\n\n An exception has occured! Image not downloaded \n\n\n")
+
 if __name__ == '__main__':
     start_time = time.time()
-    astrometry(os.getcwd())
+    api_key = 'iqzsxmsaxydxbioo'
+    client = Client()
+    client.login(api_key)
+    #Just using random sn for example here
+    astrometry(os.getcwd(), 210.910674637, 54.3116510708, client)
     end_time = time.time()
     elapsed_time = end_time - start_time
     print("Time to execute statement: " + str(elapsed_time) + 'seconds')
